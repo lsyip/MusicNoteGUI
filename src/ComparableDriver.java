9d512b5df5ba91c32621d7993424685b8ec174d5
lsyip <lsyip62@gmail.com>
2021-03-19 19:53:12 -0400

    three note frequency comparison

--- src/ComparableDriver.java
+++ src/ComparableDriver.java
@@ -1,17 +1,29 @@
+import java.util.ArrayList;
+
 public class ComparableDriver {
     public static void main(String[] args) {
         Note note1 = new Note();
-        Note note2 = new Note("whole", 0);
+        Note note2 = new Note("whole", 1);
+        Note note3 = new Note("half", -1);
 
-        int result = note1.compareTo(note2);
+        //ArrayList of Notes
+        ArrayList<Note> noteList = new ArrayList<>();
+        noteList.add(note1);
+        noteList.add(note2);
+        noteList.add(note3);
 
-        if (result < 0) {
-            System.out.println(note1.computeName(note1.getNoteValue()) + note1.computeFrequency(note1.getNoteValue()) + " is lower than " + note2.computeName(note2.getNoteValue()) + note2.computeFrequency(note2.getNoteValue()));
-        } else if (result > 0) {
-            System.out.println(note1.computeName(note1.getNoteValue()) + note1.computeFrequency(note1.getNoteValue()) + " is higher than " + note2.computeName(note2.getNoteValue()) + note2.computeFrequency(note2.getNoteValue()));
+        for (Note note : noteList) {
+            int result = note.compareTo(note1);
+
+            if (result < 0) {
+                System.out.println(note.computeName(note.getNoteValue()) + note.computeFrequency(note.getNoteValue()) + " is lower than " + note1.computeName(note1.getNoteValue()) + note1.computeFrequency(note1.getNoteValue()));
+            } else if (result > 0) {
+                System.out.println(note.computeName(note.getNoteValue()) + note.computeFrequency(note.getNoteValue()) + " is higher than " + note1.computeName(note1.getNoteValue()) + note1.computeFrequency(note1.getNoteValue()));
+            }
+            else {
+                System.out.println(note.computeName(note.getNoteValue()) + note.computeFrequency(note.getNoteValue()) + " is equal to " + note1.computeName(note1.getNoteValue()) + note1.computeFrequency(note1.getNoteValue()));
+            }
         }
-        else {
-            System.out.println(note1.computeName(note1.getNoteValue()) + note1.computeFrequency(note1.getNoteValue()) + " is equal to " + note2.computeName(note2.getNoteValue()) + note2.computeFrequency(note2.getNoteValue()));
-        }
+
     }
 }
